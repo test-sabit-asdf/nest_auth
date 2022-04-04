e71676c87928e4b7cda986d08a2df3510bb6ee09
SabiulSabit <iamsabit99@gmail.com>
2022-04-04 11:43:28 +0600

    random code added

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,13 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import jwt from 'jsonwebtoken';
+
+function f() {
+  x = 23;
+  let x;
+}
 
 async function bootstrap() {
-  const secret = 'my-secret-key';
-
-  const token = jwt.sign({ foo: 'bar' }, secret, { algorithm: 'none' });
-  jwt.verify(token, null, { algorithms: ['HS256', 'none'] });
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.useGlobalPipes(
